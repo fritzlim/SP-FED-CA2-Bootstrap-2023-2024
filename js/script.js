de20e917ae3dd5d11773448a24d5d280be9d719b
Fritz Lim <fritzlim@gmail.com>
2023-07-19 11:10:57 +0800

    fixed code indentation in index.html
    [WIP] Using 1 event handler function for multiple images. This code is temporarily commented-out in this commit.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// function showImage() {
+//   //   alert("Smile! :)");
+//   //   alert(document.getElementById("img1").name);
+
+//   // TODO: Able to use 1 function to show any image's name in an alert window.
+//   var imageName = document.getElementById("img1").name;
+
+//   switch (imageName) {
+//     case "1":
+//       alert(imageName);
+//       break;
+//   }
+// }
